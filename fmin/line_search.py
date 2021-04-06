daa74978738c7ac71f1ee482ddb4994b87fc84a1
rfeinman <rfeinman16@gmail.com>
2021-04-06 15:47:53 -0400

    update backtracking line search

--- fmin/line_search.py
+++ fmin/line_search.py
@@ -28,14 +28,18 @@ def backtracking(fun, x, t, d, f, g, mu=0.1, decay=0.98, max_ls=500, tmin=1e-5):
     Expects `fun` to take arguments {x, t, d} and return {f(x1), x1},
     where x1 is the new location after taking a step from x in direction d
     with step size t.
+
+    We use a generalized variant of the armijo condition that supports
+    arbitrary step functions x' = step(x,t,d). When step(x,t,d) = x + t * d
+    it is equivalent to the standard condition.
     """
     x_new = x
     f_new = f
-    gtd = g.mul(d).sum()
     success = True
     for i in range(max_ls):
         f_new, x_new = fun(x, t, d)
-        if f_new <= f + mu * t * gtd:
+        # generalized variant of the armijo condition
+        if f_new <= f + mu * g.mul(x_new-x).sum():
             break
         if t <= tmin:
             warnings.warn('step size has reached the minimum threshold.')
