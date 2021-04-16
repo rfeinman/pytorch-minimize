af885c203ea58ad488a9f7eeaaba47aefefa5f7d
rfeinman <rfeinman16@gmail.com>
2021-04-15 22:28:33 -0400

    update init

--- fmin/__init__.py
+++ fmin/__init__.py
@@ -2,3 +2,4 @@ from .minimize import minimize
 from .bfgs import fmin_bfgs
 from .newton import fmin_newton_cg, fmin_newton_exact
 from .trust_constr import fmin_trust_constr
+from .lstsq import least_squares
