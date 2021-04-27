41baba70626de0d7a020f5f3f114ffdc02fee012
rfeinman <rfeinman16@gmail.com>
2021-04-27 01:08:10 -0400

    update docstring

--- fmin/lstsq/least_squares.py
+++ fmin/lstsq/least_squares.py
@@ -8,6 +8,8 @@ import torch
 from .trf import trf
 from .common import EPS, in_bounds, make_strictly_feasible
 
+__all__ = ['least_squares']
+
 
 TERMINATION_MESSAGES = {
     -1: "Improper input parameters status returned from `leastsq`",
@@ -86,7 +88,7 @@ def least_squares(
         fun, x0, bounds=None, method='trf', ftol=1e-8, xtol=1e-8,
         gtol=1e-8, x_scale=1.0, tr_solver='lsmr', tr_options=None,
         max_nfev=None, verbose=0, args=(), kwargs=None):
-    """Solve a nonlinear least-squares problem with bounds on the variables.
+    r"""Solve a nonlinear least-squares problem with bounds on the variables.
 
     Given the residuals f(x) (an m-D real function of n real
     variables) and the loss function rho(s) (a scalar function), `least_squares`
@@ -110,54 +112,29 @@ def least_squares(
     x0 : array_like with shape (n,) or float
         Initial guess on independent variables. If float, it will be treated
         as a 1-D array with one element.
-    jac : {'2-point', '3-point', 'cs', callable}, optional
-        Method of computing the Jacobian matrix (an m-by-n matrix, where
-        element (i, j) is the partial derivative of f[i] with respect to
-        x[j]). The keywords select a finite difference scheme for numerical
-        estimation. The scheme '3-point' is more accurate, but requires
-        twice as many operations as '2-point' (default). The scheme 'cs'
-        uses complex steps, and while potentially the most accurate, it is
-        applicable only when `fun` correctly handles complex inputs and
-        can be analytically continued to the complex plane. Method 'lm'
-        always uses the '2-point' scheme. If callable, it is used as
-        ``jac(x, *args, **kwargs)`` and should return a good approximation
-        (or the exact value) for the Jacobian as an array_like (np.atleast_2d
-        is applied), a sparse matrix (csr_matrix preferred for performance) or
-        a `scipy.sparse.linalg.LinearOperator`.
     bounds : 2-tuple of array_like, optional
         Lower and upper bounds on independent variables. Defaults to no bounds.
         Each array must match the size of `x0` or be a scalar, in the latter
         case a bound will be the same for all variables. Use ``np.inf`` with
         an appropriate sign to disable bounds on all or some variables.
-    method : {'trf', 'dogbox', 'lm'}, optional
+    method : str, optional
         Algorithm to perform minimization.
             * 'trf' : Trust Region Reflective algorithm, particularly suitable
               for large sparse problems with bounds. Generally robust method.
             * 'dogbox' : dogleg algorithm with rectangular trust regions,
               typical use case is small problems with bounds. Not recommended
               for problems with rank-deficient Jacobian.
-            * 'lm' : Levenberg-Marquardt algorithm as implemented in MINPACK.
-              Doesn't handle bounds and sparse Jacobians. Usually the most
-              efficient method for small unconstrained problems.
         Default is 'trf'. See Notes for more information.
     ftol : float or None, optional
-        Tolerance for termination by the change of the cost function. Default
-        is 1e-8. The optimization process is stopped when ``dF < ftol * F``,
+        Tolerance for termination by the change of the cost function. The
+        optimization process is stopped when ``dF < ftol * F``,
         and there was an adequate agreement between a local quadratic model and
-        the true model in the last step.
-        If None and 'method' is not 'lm', the termination by this condition is
-        disabled. If 'method' is 'lm', this tolerance must be higher than
-        machine epsilon.
+        the true model in the last step. If None, the termination by this
+        condition is disabled. Default is 1e-8.
     xtol : float or None, optional
         Tolerance for termination by the change of the independent variables.
-        Default is 1e-8. The exact condition depends on the `method` used:
-            * For 'trf' and 'dogbox' : ``norm(dx) < xtol * (xtol + norm(x))``.
-            * For 'lm' : ``Delta < xtol * norm(xs)``, where ``Delta`` is
-              a trust-region radius and ``xs`` is the value of ``x``
-              scaled according to `x_scale` parameter (see below).
-        If None and 'method' is not 'lm', the termination by this condition is
-        disabled. If 'method' is 'lm', this tolerance must be higher than
-        machine epsilon.
+        Termination occurs when ``norm(dx) < xtol * (xtol + norm(x))``.
+        If None, the termination by this condition is disabled. Default is 1e-8.
     gtol : float or None, optional
         Tolerance for termination by the norm of the gradient. Default is 1e-8.
         The exact condition depends on a `method` used:
@@ -167,12 +144,6 @@ def least_squares(
             * For 'dogbox' : ``norm(g_free, ord=np.inf) < gtol``, where
               ``g_free`` is the gradient with respect to the variables which
               are not in the optimal state on the boundary.
-            * For 'lm' : the maximum absolute value of the cosine of angles
-              between columns of the Jacobian and the residual vector is less
-              than `gtol`, or the residual vector is zero.
-        If None and 'method' is not 'lm', the termination by this condition is
-        disabled. If 'method' is 'lm', this tolerance must be higher than
-        machine epsilon.
     x_scale : array_like or 'jac', optional
         Characteristic scale of each variable. Setting `x_scale` is equivalent
         to reformulating the problem in scaled variables ``xs = x / x_scale``.
@@ -185,25 +156,17 @@ def least_squares(
         [JJMore]_).
     max_nfev : None or int, optional
         Maximum number of function evaluations before the termination.
-        If None (default), the value is chosen automatically:
-            * For 'trf' and 'dogbox' : 100 * n.
-            * For 'lm' :  100 * n if `jac` is callable and 100 * n * (n + 1)
-              otherwise (because 'lm' counts function calls in Jacobian
-              estimation).
-    tr_solver : {None, 'exact', 'lsmr'}, optional
-        Method for solving trust-region subproblems, relevant only for 'trf'
-        and 'dogbox' methods.
+        If None (default), the value is chosen as 100 * n.
+    tr_solver : str, optional
+        Method for solving trust-region subproblems.
             * 'exact' is suitable for not very large problems with dense
               Jacobian matrices. The computational complexity per iteration is
               comparable to a singular value decomposition of the Jacobian
               matrix.
             * 'lsmr' is suitable for problems with sparse and large Jacobian
-              matrices. It uses the iterative procedure
-              `scipy.sparse.linalg.lsmr` for finding a solution of a linear
-              least-squares problem and only requires matrix-vector product
-              evaluations.
-        If None (default), the solver is chosen based on the type of Jacobian
-        returned on the first iteration.
+              matrices. It uses an iterative procedure for finding a solution
+              of a linear least-squares problem and only requires matrix-vector
+              product evaluations.
     tr_options : dict, optional
         Keyword options passed to trust-region solver.
             * ``tr_solver='exact'``: `tr_options` are ignored.
@@ -212,16 +175,15 @@ def least_squares(
               (bool, default is True), which adds a regularization term to the
               normal equation, which improves convergence if the Jacobian is
               rank-deficient [Byrd]_ (eq. 3.4).
-    verbose : {0, 1, 2}, optional
-        Level of algorithm's verbosity:
-            * 0 (default) : work silently.
+    verbose : int, optional
+        Level of algorithm's verbosity.
+            * 0 : work silently (default).
             * 1 : display a termination report.
             * 2 : display progress during iterations (not supported by 'lm'
               method).
     args, kwargs : tuple and dict, optional
-        Additional arguments passed to `fun` and `jac`. Both empty by default.
-        The calling signature is ``fun(x, *args, **kwargs)`` and the same for
-        `jac`.
+        Additional arguments passed to `fun`. Both empty by default.
+        The calling signature is ``fun(x, *args, **kwargs)``.
 
     """
     if tr_options is None:
