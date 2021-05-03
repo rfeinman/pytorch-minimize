e654dce25eae34d96a6538fea27b1f382d787849
rfeinman <rfeinman16@gmail.com>
2021-05-03 02:34:24 -0400

    adding alternative versions for the krylov subspace solver

--- torchmin/trustregion/krylov.py
+++ torchmin/trustregion/krylov.py
@@ -1,6 +1,8 @@
 """
 TODO: this module is not yet complete. It is not ready for use.
 """
+import numpy as np
+from scipy.linalg import eigh_tridiagonal, get_lapack_funcs
 import torch
 try:
     # todo: port these functions from private "ptkit" library
@@ -21,26 +23,150 @@ class KrylovSubproblem(BaseQuadraticSubproblem):
     .. [1] F. Lenders, C. Kirches, and A. Potschka, "trlib: A vector-free
            implementation of the GLTR method for...",
            arXiv:1611.04718.
-    .. [2] J. Nocedal and  S. Wright, "Numerical optimization",
+    .. [2] N. Gould, S. Lucidi, M. Roma, P. Toint: “Solving the Trust-Region
+           Subproblem using the Lanczos Method”,
+           SIAM J. Optim., 9(2), 504–525, 1999.
+    .. [3] J. Nocedal and  S. Wright, "Numerical optimization",
            Springer Science & Business Media. pp. 83-91, 2006.
     """
     hess_prod = True
 
     # extra variable defs
-    tol = 1e-5
     lambd_0 = 1e-3
     max_lanczos = None
     max_ms_iters = 500  # max iterations of the Moré-Sorensen loop
 
-    def __init__(self, x, fun, k_easy=0.1, k_hard=0.2, ortho=True, debug=False):
+    def __init__(self, x, fun, k_easy=0.1, k_hard=0.2, tol=1e-5, ortho=True, debug=False):
         super().__init__(x, fun)
         self.eps = torch.finfo(x.dtype).eps
         self.k_easy = k_easy
         self.k_hard = k_hard
+        self.tol = tol
         self.ortho = ortho
         self.nlanczos = 0
         self.best_obj = float('inf')
         self._debug = debug
+
+    def solve_krylov_mod2(self, Ta, Tb, gamma_0, tr_radius):
+        """This version avoids factorization altogether and instead uses
+        the very fast banded solve routine.
+
+        Based on Algorithm 5.2 of [2]_ (but modified to avoid factorize)
+        """
+        device, dtype = Ta.device, Ta.dtype
+
+        # convert to numpy
+        Ta = Ta.cpu().numpy()
+        Tb = Tb.cpu().numpy()
+        tr_radius = float(tr_radius)
+
+        # right hand side
+        rhs = np.zeros_like(Ta)
+        rhs[0] = - float(gamma_0)
+
+        # estimate smallest eigenvalue
+        eig0 = eigh_tridiagonal(
+            Ta, Tb, eigvals_only=True, select='i',
+            select_range=(0,0), lapack_driver='stebz')
+        eig0 = eig0.item()
+        lambd_lb = max(-eig0, 0) + 1e-3
+        if self._debug:
+            print('lambda_lb: %0.4e' % lambd_lb, end=' - ')
+
+        # get LAPACK solver for sym-PD banded matrices
+        ptsv, = get_lapack_funcs(('ptsv',), (Ta, Tb, rhs))
+
+        lambd = self.lambd_0
+        for _ in range(self.max_ms_iters):
+            lambd = max(lambd, lambd_lb)
+            Ta_k = Ta + lambd
+            _, _, p, info = ptsv(Ta_k, Tb, rhs)
+            assert info == 0
+            p_norm = np.linalg.norm(p)
+            if p_norm < tr_radius:
+                if self._debug:
+                    print('nlanczos=%4d: solution found' % self.nlanczos)
+                status = 0
+                break
+            elif abs(p_norm - tr_radius) / tr_radius <= self.k_easy:
+                if self._debug:
+                    print('nlanczos=%4d: relative error reached' % self.nlanczos)
+                status = 1
+                break
+            _, _, v, info = ptsv(Ta_k, Tb, p)
+            assert info == 0
+            lambd += (p_norm**2 / v.dot(p)) * (p_norm - tr_radius) / tr_radius
+        else:
+            if self._debug:
+                print('nlanczos=%d: krylov search did not converge' % self.nlanczos)
+            status = -1
+
+        p = torch.tensor(p, device=device, dtype=dtype)
+
+        return p, status, lambd
+
+
+    def solve_krylov_mod1(self, Ta, Tb, gamma_0, tr_radius):
+        """This version uses the unit bi-diagonal factorization schema for T
+
+        Based on Algorithm 5.2 of [2]_
+        """
+        device, dtype = Ta.device, Ta.dtype
+
+        # convert to numpy
+        Ta = Ta.cpu().numpy()
+        Tb = Tb.cpu().numpy()
+        tr_radius = float(tr_radius)
+
+        # right hand side
+        rhs = np.zeros_like(Ta)
+        rhs[0] = - float(gamma_0)
+
+        # prepare functions for factorizing and solving sym-PD tridiagonal
+        factor_td, solve_td = get_lapack_funcs(('pttrf', 'pttrs'), (Ta, Tb))
+
+        def solve_unit_bidiag(ee, pp):
+            raise NotImplementedError  # TODO
+
+        # estimate smallest eigenvalue
+        eig0 = eigh_tridiagonal(
+            Ta, Tb, eigvals_only=True, select='i',
+            select_range=(0,0), lapack_driver='stebz')
+        eig0 = eig0.item()
+        lambd_lb = max(-eig0, 0) + 1e-3
+        if self._debug:
+            print('lambda_lb: %0.4e' % lambd_lb, end=' - ')
+
+        lambd = self.lambd_0
+        for _ in range(self.max_ms_iters):
+            lambd = max(lambd, lambd_lb)
+            d, e, info = factor_td(Ta+lambd, Tb)
+            assert info == 0
+            p, info = solve_td(d, e, rhs)
+            assert info == 0
+            p_norm = np.linalg.norm(p)
+            if p_norm < tr_radius:
+                if self._debug:
+                    print('nlanczos=%4d: solution found' % self.nlanczos)
+                # TODO: add extra checks
+                status = 0
+                break
+            elif abs(p_norm - tr_radius) / tr_radius <= self.k_easy:
+                if self._debug:
+                    print('nlanczos=%4d: relative error reached' % self.nlanczos)
+                status = 1
+                break
+            q = solve_unit_bidiag(e, p)  # TODO
+            q_norm = np.linalg.norm(q)
+            lambd += (p_norm / q_norm)**2 * (p_norm - tr_radius) / tr_radius
+        else:
+            if self._debug:
+                print('nlanczos=%d: krylov search did not converge' % self.nlanczos)
+            status = -1
+
+        p = torch.tensor(p, device=device, dtype=dtype)
+
+        return p, status, lambd
 
     def solve_krylov(self, Ta, Tb, gamma_0, tr_radius):
         """Solve the trust-region sub-problem within a Krylov subspace
@@ -141,7 +267,7 @@ class KrylovSubproblem(BaseQuadraticSubproblem):
             self.nlanczos += 1
 
             # GLTR sub-problem
-            h, status, lambd = self.solve_krylov(a[:i+1], b[:i], gamma_0, tr_radius)
+            h, status, lambd = self.solve_krylov_mod2(a[:i+1], b[:i], gamma_0, tr_radius)
 
             if status >= 0:
                 # project p back to R^n
