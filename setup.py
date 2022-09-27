0c9945d9e58fb01e8519c51e43b88631017697f6
rfeinman <rfeinman16@gmail.com>
2022-09-27 17:30:52 -0400

    moving all package metadata into setup.py; remove redundant info from pyproject.toml

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
+# read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+# find packages
 packages = find_packages(exclude=("tests", "tests.*"))
 
+# setup
 setup(
     name='pytorch-minimize',
     version='0.0.1',
     description='Newton and Quasi-Newton optimization with PyTorch',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://pytorch-minimize.readthedocs.io',
     author='Reuben Feinman',
     author_email='reuben.feinman@nyu.edu',
