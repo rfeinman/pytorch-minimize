c192e7e7823c4abb79e012438d421a2874d3b809
rfeinman <rfeinman16@gmail.com>
2022-09-27 10:45:49 -0400

    adding more detail to setup()

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Newton and Quasi-Newton optimization with PyTorch',
     url='https://pytorch-minimize.readthedocs.io',
     author='Reuben Feinman',
-    author_email='',
+    author_email='reuben.feinman@nyu.edu',
     license='MIT Licence',
     packages=['torchmin'],
     zip_safe=False,
@@ -14,5 +14,11 @@ setup(
         'numpy>=1.18.0',
         'scipy>=1.6',
         'torch>=1.9.0'
+    ],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ]
 )
