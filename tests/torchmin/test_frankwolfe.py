7465af15b2da2bc3d702d6e0a47c84ff83005885
Calvin McCarter <calvin199@gmail.com>
2026-01-14 16:31:51 -0500

    Constrained minimization via the Frank-Wolfe method (#28)
    * constrained minimization via Frank-Wolfe method
    
    * rename file to frankwolfe.py
    
    * rename frankwolfe functions (preceeding underscore); remove experimental frankwolfe functions from `torchmin` module until further testing; update test script
    
    * rename test script to test_frankwolfe.py
    
    ---------
    
    Co-authored-by: Reuben Feinman <9561213+rfeinman@users.noreply.github.com>

---
+++ tests/torchmin/test_frankwolfe.py
@@ -0,0 +1,38 @@
+import pytest
+import torch
+
+from torchmin.frankwolfe import (
+    _minimize_constr_birkhoff_polytope,
+    _minimize_constr_tracenorm,
+)
+
+
+def test_birkhoff_polytope():
+    n, d = 5, 10
+    X = torch.randn(n, d)
+    Y = torch.flipud(torch.eye(n)) @ X
+    def fun(P):
+        return torch.sum((X @ X.T @ P - P @ Y @ Y.T) ** 2)
+
+    init_P = torch.eye(n)
+    init_err = torch.sum((X - init_P @ Y) ** 2)
+    res = _minimize_constr_birkhoff_polytope(fun, init_P)
+    est_P = res.x
+    final_err = torch.sum((X - est_P @ Y) ** 2)
+    torch.testing.assert_close(est_P.sum(0), torch.ones(n))
+    torch.testing.assert_close(est_P.sum(1), torch.ones(n))
+    assert final_err < 0.01 * init_err
+
+
+def test_tracenorm():
+    def fun(X):
+        return torch.sum((X - torch.eye(5)) ** 2)
+
+    init_X = torch.zeros((5, 5))
+    res = _minimize_constr_tracenorm(fun, init_X, 5.)
+    est_X = res.x
+    torch.testing.assert_close(est_X, torch.eye(5), rtol=1e-2, atol=1e-2)
+
+    res = _minimize_constr_tracenorm(fun, init_X, 1.)
+    est_X = res.x
+    torch.testing.assert_close(est_X, 0.2*torch.eye(5), rtol=1e-2, atol=1e-2)
